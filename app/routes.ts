82d7fa38402a0c3ac309538dbd3e4e1355b088c4
Brooks Lybrand <brookslybrand@gmail.com>
2024-09-17 10:07:39 -0500

    Add an example with @react-router/remix-config-routes-adapter

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,15 @@ import { flatRoutes } from "@react-router/fs-routes";
 
 import { routes as rrRoutes } from "./rr-routes/routes";
 
+// Support for custom route conventions in Remix
+//
+// import { remixConfigRoutes } from "@react-router/remix-config-routes-adapter";
+// import { flatRoutes } from "remix-flat-routes";
+//
+// export const routes: RouteConfig = remixConfigRoutes(async defineRoutes => {
+//   return flatRoutes('routes', defineRoutes);
+// });
+
 export const routes: RouteConfig = [
   ...(await flatRoutes()),
   ...(await rrRoutes),
