f7276067b79574065cd0daccfc88939a2b61616c
Brooks Lybrand <brookslybrand@gmail.com>
2025-04-17 15:45:30 -0400

    dashboard routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import type { RouteConfig } from "@react-router/dev/routes";
 import { route, layout, index } from "@react-router/dev/routes";
 import authRoutes from "./auth/routes";
+import dashboardRoutes from "./dashboard/routes";
 
 let routes = [
   layout("layouts/public.tsx", [
@@ -9,25 +10,7 @@ let routes = [
     route("contact", "pages/contact.tsx"),
   ]),
   ...authRoutes,
-  route("dashboard", "dashboard/pages/dashboard-home.tsx"),
-  route("dashboard/calendar", "dashboard/layouts/calendar.tsx", [
-    index("dashboard/pages/calendar.tsx"),
-    route(":day", "dashboard/pages/calendar-day.tsx"),
-  ]),
-  route("dashboard/projects", "dashboard/layouts/projects.tsx", [
-    route(":projectId", "dashboard/layouts/project-details.tsx", [
-      index("dashboard/pages/project-details.tsx"),
-      route("collaborators", "dashboard/pages/project-collaborators.tsx"),
-      route("edit", "dashboard/pages/project-edit.tsx"),
-      route("settings", "dashboard/pages/project-settings.tsx"),
-      route("tasks/:taskId", "dashboard/pages/project-task.tsx"),
-    ]),
-    route("new", "dashboard/pages/project-new.tsx"),
-  ]),
-  route(
-    "dashboard/projects/:projectId/print",
-    "dashboard/pages/project-print.tsx",
-  ),
+  ...dashboardRoutes,
 ] satisfies RouteConfig;
 
 export default routes;
